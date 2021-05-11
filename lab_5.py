e6f9879cd9795fa3904471053ee7823bbfe24dd7
unknown <rutilioagomez1@mail.fresnostate.edu>
2021-05-11 02:23:34 -0700

    pseudocode

--- lab_5.py
+++ lab_5.py
@@ -15,3 +15,24 @@ in our dictionary
 After eaxh word is translated, we then
 Print out the translated sentence to the user.
 """
+
+
+"""
+main():
+    set sentence = input()
+    set dictionary = create_dictionary()
+    translate(sentence, dictionary)
+
+translate(sentence, dictionary):
+    words = for each of the word in the sentence
+    for each words, translate the word
+    print translates sentence to user
+
+create_dictionary():
+    read in textese.txt
+    create list = each line from file
+    create a dict off of the list
+    return the dict
+
+main()
+"""
