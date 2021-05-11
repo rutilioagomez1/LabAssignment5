e9d193d696e9add2396cedfb3d811568aea17832
unknown <rutilioagomez1@mail.fresnostate.edu>
2021-05-11 02:18:21 -0700

    finished structured english

--- lab_5.py
+++ lab_5.py
@@ -1,6 +1,3 @@
-
-
-
 """
 This program is trying to translate a sentence captured as user input.
 We first read in the text file textese.txt.
@@ -18,42 +15,3 @@ in our dictionary
 After eaxh word is translated, we then
 Print out the translated sentence to the user.
 """
-
-"""
-main():
-    set sentence = input()
-    set dictionary = create_dictionary()
-    translate(sentence, dictionary)
-
-
-translate(sentence, dictionary):
-    words = for each of the word in the sentence
-    for each words, translate the word
-    print translates sentence to user
-
-create_dictionary():
-    read in textese.txt
-    create list = each line from file
-    create a dict off of the list
-    return the dict
-
-main()
-"""
-
-def main():
-    sentence = input("Enter a sentence: ")
-    dictionary = create_dictionary("textese.txt")
-    translate(sentence, dictionary)
-
-def create_dictionary(txt_file):
-    infile = open(txt_file, "r")
-    words = [word.rstrip() for word in infile]
-    infile.close()
-    return dict([word.split(",") for word in words]) # "tonight,2nite"
-
-def translate(sentence, dictionary):
-    words = sentence.split() 
-    for word in words:
-        print(dictionary.get(word, word), " ", end="")
-
-main()
